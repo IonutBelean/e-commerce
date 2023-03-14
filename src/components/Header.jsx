79da888cc0618973df5b79556ff2f3ace6d3df85
IonutBelean <ionutbelean4@gmail.com>
2023-03-14 19:06:18 +0200

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,30 @@
-import { Nav, Navbar, Container } from "react-bootstrap";
+import { Nav, Navbar, Container, Form, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <Navbar bg="light" expand="lg">
+    <Navbar expand="lg">
       <Container>
         <Navbar.Brand as={Link} to="/">
-          React-Bootstrap
+          UnderFive
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
+            <Form className="d-flex">
+              <Form.Control
+                type="search"
+                placeholder="Search"
+                className="me-2"
+                aria-label="Search"
+              />
+              <Button variant="outline-success">Search</Button>
+            </Form>
             <Nav.Link as={Link} to="/">
-              Home
+              Favourite
             </Nav.Link>
-            <Nav.Link as={Link} to="/Products">
-              Products
+            <Nav.Link as={Link} to="/">
+              Chart
             </Nav.Link>
           </Nav>
         </Navbar.Collapse>
--- src/components/HeaderSecond.jsx
+++ src/components/HeaderSecond.jsx
@@ -5,9 +5,6 @@ const HeaderSecond = () => {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand as={Link} to="/">
-          React-Bootstrap
-        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
